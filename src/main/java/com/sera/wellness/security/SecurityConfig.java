cbb55d3f5b29bdfe8971e429e2be15deb5ea7473
Самат <samat.zay@mail.ru>
2019-04-19 13:11:02 +0300

    add security

---
+++ src/main/java/com/sera/wellness/security/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.sera.wellness.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  @Autowired
+  @Qualifier(value = "customUserDetailsService")
+  private UserDetailsService userDetailsService;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/signup").permitAll()
+            .antMatchers("/articles").authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/signin")
+            .usernameParameter("email")
+            .passwordParameter("password")
+            .failureUrl("/signin?error")
+            .permitAll()
+            .defaultSuccessUrl("/articles")
+            .and()
+            .logout().logoutSuccessUrl("/signin")
+            .permitAll();
+  }
+  @Bean
+  public DaoAuthenticationProvider authProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setPasswordEncoder(passwordEncoder());
+    authProvider.setUserDetailsService(userDetailsService);
+
+    return authProvider;
+  }
+
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(authProvider());
+  }
+}
