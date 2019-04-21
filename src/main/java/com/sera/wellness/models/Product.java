071e06dddfb7244f6312af47b10e46b6829f3c7a
galeev <galeevrusteam@gmail.com>
2019-04-21 14:21:36 +0300

    .

---
+++ src/main/java/com/sera/wellness/models/Product.java
@@ -0,0 +1,28 @@
+package com.sera.wellness.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Product")
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+    private String name,
+            img;
+    private Float protein,
+            fats,
+            carbohydrates,
+            calories;
+    private Boolean type;
+}
